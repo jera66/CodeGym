69c920cd7b198af410b0e2031041a1c783d6e6a3
Jerathel Czerny <jeratheljean@yahoo.com>
2024-11-10 00:21:10 -0500

    Second commit

--- Solution2.java
+++ Solution2.java
@@ -1,3 +1,20 @@
+// Write a program that displays "Ellie is very smart" 5 times. Each time, on a new line.
+
+// Example output:
+// Ellie is very smart
+// Ellie is very smart
+// Ellie is very smart
+// Ellie is very smart
+// Ellie is very smart
+
+// Requirements:
+// •	The program should output text.
+// •	The text should begin with "Ellie".
+// •	The text should end with "smart".
+// •	The text should consist of 5 lines.
+// •	The displayed text must match the task conditions.
+
+
 public class Solution2 {
     public static void main(String[] args) {
         //write your code here
---
+++ Solution4.java
@@ -0,0 +1,21 @@
+// Write a program that declares the following variables in the main method:
+// String name, int age, and String city.
+
+// Note: "Declaring a variable" is the same thing as "creating a variable".
+
+// Requirements:
+// •	Declare a String variable called name.
+// •	Declare an int variable called age.
+// •	Declare a String variable called city.
+// •	There should be 3 variables declared.
+
+
+
+public class Solution4 {
+    public static void main(String[] args) {
+        //write your code here
+        String name;
+        int age;
+        String city;
+    }
+}
